d40b4d489c59eed401c06ff64861975b80941caf
İlayda Yıldız <146078544+ilaydayilidz778@users.noreply.github.com>
2024-02-08 19:25:52 +0300

    Add project files.

---
+++ TraversalCoreProject.EntityLayer/Concrete/About.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProject.EntityLayer.Concrete
+{
+    public class About
+    {
+        public int Id { get; set; }
+        public string Details { get; set; } = null!;
+        public string ImageUrl { get; set; } = null!;
+        public bool Status { get; set; }
+    }
+}
